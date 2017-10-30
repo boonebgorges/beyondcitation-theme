b3d7f17a2d0a2ec78502405a8fb627713c75a62c
Boone B Gorges <boonebgorges@gmail.com>
2017-10-30 14:17:03 -0500

    Add missing template.

---
+++ db-field.php
@@ -0,0 +1,19 @@
+<?php
+
+$field_group_slug = $GLOBALS['field_group_slug'];
+$field_group_info = $GLOBALS['field_groups'][ $field_group_slug ];
+
+$field_slug = $GLOBALS['field_slug'];
+$field_info = $GLOBALS['bc_fields'][ $field_slug ];
+
+$field_data = get_post_meta( get_the_ID(), $field_slug, true );
+
+if ( $field_data ) {
+	if ( ! $field_group_info['is_single'] ) {
+		$field_data = '<strong>' . esc_html( $field_info['title'] ) . ':</strong> ' . $field_data;
+	}
+
+	$field_data = wpautop( $field_data );
+
+	echo $field_data;
+}
